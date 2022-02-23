f27a7d9fa63363b8714e03eaea68a288871c61d2
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2022-02-23 15:14:02 -0500

    Improve documentation for TCGAData

--- TCGAData.py
+++ TCGAData.py
@@ -1,6 +1,8 @@
 import os.path as osp
 import time
 
+from typing import Union, Optional, Callable
+
 import pickle
 import gzip
 
@@ -12,21 +14,65 @@ from build_graph_ref import read_data
 
 
 class TCGADataset(InMemoryDataset):
+    """A dataset of samples from The Cancer Genome Atlas (TCGA).
+
+    This reads TCGA data files in HDF format from a given `root` folder. The file names
+    are either explicitly given in the constructor, or are read from a text file (see
+    `files` below).
+    
+    Each TCGA file is expected to contain an encoded version of the mutated genes from
+    each sample, together with a string label. This label is converted to an integer
+    using the `label_mapping` (see below). The data is converted to a graph in which
+    each mutated gene is a node, with the encoded data from the TCGA file used as node
+    attributes. The edge identities and weights are copied from the `gene_graph` (see
+    below). The data is `pre_transform`ed after loading, and then saved in a subfolder
+    of the root folder whose name starts with "processed" and is potentially followed by
+    "_" and the `suffix`, if there is one. The filename used for the processed data is
+    "data.pt". The `pre_transform` and `pre_filter` that were used are also stored in
+    this folder, with names `"pre_transform.pt"` and `"pre_filter.pt"`, respectively.
+    (`pre_filter` is actually not currently used here, but `torch_geometric`'s `Dataset`
+    generates the `.pt` file anyway.)
+
+    The `gene_graph` is expected to be a gzip-compressed tab-separated file with rows of
+    the form `(gene_symbol1, gene_symbol2, weight)`, encoding a graph. This is read and
+    symmetrized (i.e., edges are added for both `(gene_symbol1, gene_symbol2)` and
+    `(gene_symbol2, gene_symbol1`)), and the result is stored in a pickle inside the
+    root folder for faster access in future runs.
+    
+    :param root: root folder for the samples; also used to store processed data
+    :param label_mapping: dictionary from sample labels in `str` or `bytes` format to
+        numeric labels; this can also be a list, in which case each element is mapped to
+        its index in the list
+    :param files: either a list of data file names (in HDF format), or the name of a
+        single text file containing the names of the data files; by default, the list of
+        files names is loaded from a file called `samples.txt` in the root folder (but
+        see the `samples_file` option)
+    :param transform: transformation applied at each access
+    :param pre_transform: transformation applied before saving to disk
+    :param name: name for the dataset; default: "tcga_" followed by the root's
+        `basename` and the `suffix`, if there is one, preceded by "_"
+    :param suffix: suffix used for generating the name of the processed-file folder, and
+        also for setting the default `name`
+    :param gene_graph: filename for the gene graph in root folder
+    :param samples_file: file in the root folder where the sample names are read from
+    """
+
     def __init__(
         self,
-        root,
-        label_mapping,
-        files=None,
-        transform=None,
-        pre_transform=None,
-        name=None,
-        suffix="",
-        gene_graph="gene_graph.gz",
-        samples_file="samples.txt",
+        root: str,
+        label_mapping: Union[list, dict],
+        files: Union[str, list, None] = None,
+        transform: Optional[Callable] = None,
+        pre_transform: Optional[Callable] = None,
+        name: Optional[str] = None,
+        suffix: str = "",
+        gene_graph: str = "gene_graph.gz",
+        samples_file: str = "samples.txt",
     ):
         self.suffix = suffix
         self.gene_graph = gene_graph
 
+        # handle defaults
         if name is not None:
             self.name = name
         else:
@@ -40,18 +86,21 @@ class TCGADataset(InMemoryDataset):
         if isinstance(files, (list, tuple)):
             self.files = files
         else:
+            # read list of files from file
             with open(files, "rt") as f:
                 self.files = [_.strip() for _ in f.readlines()]
                 self.files = [_ for _ in self.files if len(_) > 0]
 
+        # handle the two formats for the label mapping
         if isinstance(label_mapping, dict):
             self.label_mapping = label_mapping
         else:
             self.label_mapping = {_: i for i, _ in enumerate(label_mapping)}
 
+        # the base-class constructor will generate the processed file if it is missing
         super(TCGADataset, self).__init__(root, transform, pre_transform)
 
-        # the base-class constructor generates the processed file if it is missing
+        # load the processed dataset
         self.data, self.slices = torch.load(self.processed_paths[0])
 
     @property
@@ -64,6 +113,7 @@ class TCGADataset(InMemoryDataset):
 
     @property
     def processed_dir(self) -> str:
+        # the name of the processed folder can be changed by using the `suffix`
         processed_name = "processed"
         if len(self.suffix) > 0:
             processed_name += "_" + self.suffix
@@ -124,6 +174,4 @@ class TCGADataset(InMemoryDataset):
         torch.save((self.data, self.slices), self.processed_paths[0])
 
     def __repr__(self):
-        return 'TCGADataset(name={}, len={}, suffix="{}")'.format(
-            self.name, len(self), self.suffix
-        )
+        return f'TCGADataset(name={self.name}, len={len(self)}, suffix="{self.suffix}")'
