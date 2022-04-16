dbd1d88eb783dba48725f070ba6356bf81aec193
Zijun Zhang <zzj.zju@gmail.com>
2022-04-16 00:42:30 -0400

    network entrezID->geneSymbolconversion script

--- 01-network_id_conversion.py
+++ 01-network_id_conversion.py
@@ -1,8 +1,11 @@
 import mygene
 import gzip
+import sys
 
-network_fp = "graph/global.gz"
-
+#network_fp = "graph/global.gz"
+network_fp = sys.argv[1]
+out_fp = sys.argv[2]
+print(f"converting {network_fp} to {out_fp}")
 
 entrez_network = {}
 with gzip.GzipFile(network_fp, "r") as f:
@@ -35,7 +38,8 @@ print(
     % (len(entrez_network), len(symbol_network))
 )
 
-with gzip.GzipFile("graph/global.geneSymbol.gz", "w") as fo:
+#with gzip.GzipFile("graph/global.geneSymbol.gz", "w") as fo:
+with gzip.GzipFile(out_fp, "w") as fo:
     for g1, g2 in symbol_network:
         fo.write(("%s\t%s\t%s\n" % (g1, g2, symbol_network[(g1, g2)])).encode("utf-8"))
 
