c27a9ed8e3fb7e680133b466a855f8ca7550aa4c
Tiberiu Tesileanu <ttesileanu@gmail.com>
2022-09-20 23:03:40 -0400

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="cancernet",
-    version="0.0.5",
+    version="0.1.0",
     url="https://github.com/ttesileanu/cancer-net",
     packages=["cancernet"],
-    install_requires=[
-        "setuptools",
-        "black",
-        "cudatoolkit>=11.3",
-        "torch>=1.11",
-        "torchvision",
-        "torchaudio",
-        "pytorch-lightning>=1.6",
-        "pyg",
-        "pysr",
-        "mygene",
-        "numpy",
-        "scipy",
-        "tqdm",
-        "pandas",
-        "scikit-learn",
-        "h5py",
-        "wandb",
-        "optuna",
-        "matplotlib",
-        "seaborn",
-        "jupyter",
-    ],
 )
