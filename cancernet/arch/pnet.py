86660ede1392a869a9b95b0d85e912df1af10b90
Chris-Pedersen <c.pedersen@nyu.edu>
2023-03-02 22:35:26 -0500

    Add weighted sum of intermediate layer outputs to pnet during training

--- cancernet/arch/pnet.py
+++ cancernet/arch/pnet.py
@@ -32,40 +32,6 @@ class FeatureLayer(torch.nn.Module):
         x = x + self.bias
         return x
 
-class DiagonalLayer(torch.nn.Module):
-    """This layer will take our input data of size `(N_genes, N_features)`, and perform
-    elementwise multiplication of the features of each gene. This is effectively
-    collapsing the `N_features dimension`, outputting a single scalar latent variable
-    for each gene.
-    """
-
-    def __init__(self, num_genes: int, num_features: int):
-        super().__init__()
-        self.num_genes = num_genes
-        self.num_features = num_features
-        input_dimension = self.num_genes * self.num_features
-        self.shape = (input_dimension, self.num_genes)
-        rows = np.arange(input_dimension)
-        cols = np.arange(self.num_genes)
-        cols = np.repeat(cols, self.num_features)
-        nonzero_ind = np.column_stack((rows, cols))
-        self.register_buffer(
-            "nonzero_indices", torch.LongTensor(nonzero_ind)
-        )
-        weights = torch.Tensor(self.num_genes*self.num_features)
-        self.weights = nn.Parameter(weights)
-        self.bias = nn.Parameter(torch.Tensor(self.num_genes))
-        # initialise weights using a normal distribution; can also try uniform
-        torch.nn.init.normal_(self.weights, mean=0.0, std=1.0)
-        torch.nn.init.normal_(self.bias, mean=0.0, std=0.1)
-
-    def forward(self, x):
-        sparse_tensor = scatter_nd(self.nonzero_indices, self.weights, self.shape)
-        x = torch.mm(x, sparse_tensor)
-        # no bias yet
-        x = x + self.bias
-        return x
-
 
 class SparseLayer(torch.nn.Module):
     """Sparsely connected layer, with connections taken from pnet."""
@@ -90,15 +56,14 @@ class SparseLayer(torch.nn.Module):
         x = x + self.bias
         return x
 
-
+    
 class PNet(BaseNet):
     """Implementation of the pnet sparse feedforward network in torch. Uses the same
     pytorch geometric dataset as the message passing networks.
     """
 
-    def __init__(self, layers, num_genes: int, num_features: int, lr: float = 0.001):
+    def __init__(self, layers, num_genes: int, num_features: int=3, lr: float = 0.001, intermediate_outputs: bool=True):
         """Initialize.
-
         :param layers: list of pandas dataframes describing the pnet masks for each
             layer
         :param num_genes: number of genes in dataset
@@ -110,51 +75,78 @@ class PNet(BaseNet):
         self.layers = layers
         self.num_genes = num_genes
         self.num_features = num_features
+        self.intermediate_outputs = intermediate_outputs
         self.network = nn.ModuleList()
-        self.network.append(FeatureLayer(self.num_genes, self.num_features))
-        self.network.append(nn.Tanh())
-        self.network.append(nn.Dropout(p=0.5))
+        self.intermediate_outs = nn.ModuleList()
+        self.network.append(nn.Sequential(FeatureLayer(self.num_genes, self.num_features),nn.Tanh()))
+        self.loss_weights=[2, 7, 20, 54, 148, 400] ## Taken from pnet - final output layer is the last element
         for i, layer_map in enumerate(layers):
             if i != (len(layers) - 1):
-                self.network.append(SparseLayer(layer_map))
-                self.network.append(nn.Tanh())
-                self.network.append(nn.Dropout(p=0.1))
+                if i==0:
+                    ## First layer has dropout of 0.5, the rest have 0.1
+                    dropout=0.5
+                else:
+                    dropout=0.1
+                    ## Build pnet layers
+                self.network.append(
+                    nn.Sequential(nn.Dropout(p=dropout),SparseLayer(layer_map),nn.Tanh())
+                                    )
+                    ## Build layers for intermediate output
+                if self.intermediate_outputs:
+                    self.intermediate_outs.append(
+                    nn.Sequential(nn.Linear(layer_map.shape[0], 1),nn.Sigmoid())
+                                                  )
             else:
-                self.network.append(nn.Linear(layer_map.shape[0], 1))
-                self.network.append(nn.Sigmoid())
-        # final layer
-        # this is not needed because reactome always connects to one root node
-        # self.network.append(Linear(layer_map.to_numpy().shape[1], 1))
-
+                self.network.append(nn.Sequential(nn.Linear(layer_map.shape[0], 1),nn.Sigmoid()))
+                
     def forward(self, data):
         """Only uses the "node features", which in this case we just treat as a data
         vector for the sparse feedforward network.
         """
+        ## Create list for each component of the loss:
         # reshape for batching appropriate for feedfoward network
-        x = torch.reshape(
+        x=[torch.reshape(
             data.x, (int(data.batch[-1] + 1), self.num_genes, self.num_features)
-        )
-        for hidden_layer in self.network:
-            x = hidden_layer(x)
-        return x
+        )]
+        
+        ## First element in list is the final layer output
+        ## second element comes from the penultimate layer
+        ## and so on
+        x[0]=self.network[0](x[0])
+        for aa in range(1,len(self.network)-1):
+            x.append(self.intermediate_outs[aa-1](x[0]))
+            x[0]=self.network[aa](x[0])
+        x[0]=self.network[-1](x[0])
+        
+        ## Reverse list to begin with intermediate outputs
+        ## to align with self.loss_weights
+        x.reverse()
+        
+        ## Get weighted average of intermediate and final outputs during training
+        if self.intermediate_outputs and self.training:
+            avg_pred=0
+            for aa, loss_weight in enumerate(self.loss_weights[:len(self.layers)]):
+                avg_pred+=x[aa]*loss_weight
+            avg_pred/=sum(self.loss_weights[:len(self.layers)])
+            return avg_pred.squeeze()
+        else:
+            return x[-1].squeeze()
     
     def step(self, batch, kind: str) -> dict:
         """Generic step function that runs the network on a batch and outputs loss
         and accuracy information that will be aggregated at epoch end.
-
         This function is used to implement the training, validation, and test steps.
         """
         # run the model and calculate loss
-        y_hat = self(batch).squeeze()
-
+        y_hat = self(batch)
+        
         loss = self.criterion(y_hat, batch.y.to(torch.float32))
 
         # assess accuracy
-        pred = y_hat>0.5
-        correct = pred.eq(batch.y).sum().item()
+        #pred = y_hat.max(1)[1]
+        correct = ((y_hat>0.5).flatten()==batch.y).sum()
 
         total = len(batch.y)
-
         batch_dict = {
             "loss": loss,
             # correct and total will be used at epoch end
