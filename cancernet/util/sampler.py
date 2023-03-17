4d00650fd513fe98dae1eb8829bda2faa579719f
Chris-Pedersen <c.pedersen@nyu.edu>
2023-03-16 23:12:54 -0400

    Add sampler for deterministic sampling of test set

---
+++ cancernet/util/sampler.py
@@ -0,0 +1,22 @@
+import torch
+from torch import Tensor
+
+from torch.utils.data.sampler import Sampler
+from typing import Iterator, Sized
+
+class SequenceSampler(Sampler[int]):
+    r"""Samples elements sequentially, always in the same order.
+
+    Args:
+        data_source (Dataset): dataset to sample from
+    """
+    data_source: Sized
+
+    def __init__(self, data_source: Sized) -> None:
+        self.data_source = data_source
+
+    def __iter__(self) -> Iterator[int]:
+        return iter(self.data_source)
+
+    def __len__(self) -> int:
+        return len(self.data_source)
