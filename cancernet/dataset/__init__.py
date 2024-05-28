12a4556c7e2f763efc85b386d7a40a5e51b60c88
Chris-Pedersen <c.pedersen@nyu.edu>
2024-05-27 20:44:44 -0400

    Add graph dataset to init

--- cancernet/dataset/__init__.py
+++ cancernet/dataset/__init__.py
@@ -1,4 +1,4 @@
-from .pnet_dataset import PnetDataSet, BrainDataSet
+from .pnet_dataset import PnetDataSet, BrainDataSet, GraphDataSet
 from .gmt import load_gmt
 from .reactome import Reactome
 from .reactome_net import ReactomeNetwork, get_layer_maps
